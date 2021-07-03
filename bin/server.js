f881ecadf41c976dbb516252aee384d0cea281ff
BogdanLyopa <bogdanlyopa@gmail.com>
2021-07-03 16:03:19 +0300

    add mongoDB

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,18 @@
 const app = require('../app')
+require('dotenv').config()
+
+const { connectMongo } = require('../db/connection')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const start = async () => {
+  await connectMongo()
+  // const contacts = await Contacts.find({}).toArray();
+  // console.table(contacts);
+  app.listen(PORT, (error) => {
+    if (error) console.error('Error at server launch:', error)
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
+}
+
+start()
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,46 +1,59 @@
-const express = require('express')
-const router = express.Router()
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require('../../model/index')
+const express = require("express");
+const router = express.Router();
+
 const {
   validateCreateContact,
   validateUpdateContact,
-} = require('../../validation/contactsValidation')
+} = require("../../validation/contactsValidation");
+const modulesMiddelware = require("../../validation/models");
+const ObjectId = require("mongodb").ObjectID;
+router.use(modulesMiddelware);
 
-router.get('/', async (req, res, next) => {
-  const contacts = await listContacts()
-  res.status(200).json(contacts)
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await req.db.Contacts.find({}).toArray();
 
-router.get('/:contactId', async (req, res, next) => {
-  const contact = await getContactById(req.params.contactId)
+  res.status(200).json(contacts);
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  const contact = await req.db.Contacts.findOne({
+    _id: ObjectId(req.params.contactId),
+  });
   if (!contact) {
-    return res.status(404).json({ message: 'Not found' })
+    return res.status(404).json({ message: "Not found" });
   }
-  res.status(200).json(contact)
-})
+  res.status(200).json(contact);
+});
 
-router.post('/', validateCreateContact, async (req, res, next) => {
-  const contacts = await addContact(req.body)
-  res.status(201).json(contacts)
-})
+router.post("/", validateCreateContact, async (req, res, next) => {
+  const record = {
+    ...req.body,
+    ...(req.body.favorite ? {} : { favorite: false }),
+  };
+  const contacts = await req.db.Contacts.insertOne(record);
+  res.status(201).json(contacts);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  const contacts = await removeContact(req.params.contactId)
-  return res.status(200).json(contacts)
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const contacts = await req.db.Contacts.deleteOne({
+    _id: ObjectId(req.params.contactId),
+  });
+  return res.status(200).json({ status: "success" });
+});
 
-router.patch('/:contactId', validateUpdateContact, async (req, res, next) => {
-  const contacts = await updateContact(req.params.contactId, req.body)
+router.patch("/:contactId", validateUpdateContact, async (req, res, next) => {
+  const contacts = await req.db.Contacts.updateOne(
+    {
+      _id: ObjectId(req.params.contactId),
+    },
+    { $set: req.body }
+  );
   if (!contacts) {
-    return res.status(404).json({ message: 'Not found' })
+    return res.status(404).json({ message: "Not found" });
   }
-  return res.status(200).json(contacts)
-})
+  return res.status(200).json(contacts);
+});
 
-module.exports = router
+router.patch("/:contactId/favorite", async (req, res, next) => {});
+
+module.exports = router;
